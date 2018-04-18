5242ef112e425538e776b6515ddc20b71d54d529
Mahmoud Adam <mahmoud@cliqz.com>
2018-04-18 12:00:00 +0200

    Show sharing menus when clicking the menu button on the toolbar

---
+++ Cliqz/Share Menu/ShareExtensionActivities.swift
@@ -0,0 +1,17 @@
+//
+//  ShareExtensionActivities.swift
+//  Client
+//
+//  Created by Mahmoud Adam on 4/18/18.
+//  Copyright © 2018 Cliqz. All rights reserved.
+//
+
+import UIKit
+
+extension ShareExtensionHelper {
+    func getApplicationActivities() -> [UIActivity] {
+        let applicationActivities = [SettingsActivity()]
+        return applicationActivities
+    }
+
+}
