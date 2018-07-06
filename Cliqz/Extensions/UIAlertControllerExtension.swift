dc527ba3d830cfe3315145ac8b77f9d6257d30fc
Mahmoud Adam <mahmoud@cliqz.com>
2018-07-06 11:36:12 +0200

    Changed strings of restore tabs alert

---
+++ Cliqz/Extensions/UIAlertControllerExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIAlertControllerExtension.swift
+//  Client
+//
+//  Created by Mahmoud Adam on 7/6/18.
+//  Copyright © 2018 Cliqz. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController {
+    class func getRestoreTabsAlert() -> UIAlertController {
+        return UIAlertController(
+            title: NSLocalizedString("Well, this is embarrassing.", tableName: "Cliqz", comment: "Restore Tabs Prompt Title"),
+            message: NSLocalizedString("Looks like Cliqz crashed previously. Would you like to restore your tabs?", tableName: "Cliqz", comment: "Restore Tabs Prompt Description"),
+            preferredStyle: .alert
+        )
+    }
+}
