f0fa5544fe8d1cac16e2e92bd85e54d9b0888005
Mahmoud Adam <mahmoud@cliqz.com>
2018-09-14 10:10:05 +0200

    Optimize finding the matching rule for a domain by generating domain rule once instead of calculating it with every rule

--- Cliqz/Services/LogoLoader.swift
+++ Cliqz/Services/LogoLoader.swift
@@ -35,29 +35,30 @@ extension String {
 }
 
 class LogoLoader {
-	
-	private static let dbVersion = "1524118567171"
+    
+    private static let dbVersion = "1524118567171"
+    private static var _logoDB: JSON?
+    private static var logoDB: JSON? {
+        get {
+            if self._logoDB == nil {
+                if let path = Bundle.main.path(forResource: "logo-database", ofType: "json"),
+                    let jsonData = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe) as Data {
+                    self._logoDB = JSON(jsonData)
+                }
+            }
+            return self._logoDB
+        }
+        set {
+            self._logoDB = newValue
+        }
+    }
+    
 	private static let dispatchQueue = DispatchQueue(label: "com.cliqz.logoLoader", attributes: .concurrent);
 	
-	private static var _logoDB: JSON?
-	private static var logoDB: JSON? {
-		get {
-			if self._logoDB == nil {
-				if let path = Bundle.main.path(forResource: "logo-database", ofType: "json"),
-					let jsonData = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe) as Data {
-					self._logoDB = JSON(jsonData)
-				}
-			}
-			return self._logoDB
-		}
-		set {
-			self._logoDB = newValue
-		}
-	}
 	
-	class func loadLogo(_ url: String, completionBlock: @escaping (_ image: UIImage?, _ logoInfo: LogoInfo?,  _ error: Error?) -> Void) {
+    class func loadLogo(_ url: String, completionBlock: @escaping (_ image: UIImage?, _ logoInfo: LogoInfo?,  _ error: Error?) -> Void) {
         dispatchQueue.async {
-            let details = LogoLoader.fetchLogoDetails(url)
+            let details = fetchLogoDetails(url)
             if let u = details.url {
                 LogoLoader.downloadImage(u, completed: { (image, error) in
                     DispatchQueue.main.async {
@@ -72,82 +73,6 @@ class LogoLoader {
         }
 	}
 	
-	class func clearDB() {
-		self.logoDB = nil
-	}
-	
-	private class func fetchLogoDetails(_ url: String) -> LogoInfo {
-		var logoDetails = LogoInfo()
-		logoDetails.color = nil
-		logoDetails.fontSize = 16
-		var fixedURL = url
-		// TODO: Remove this crazy hack, which is done for localNews. For the next release we should change url parsing lib to https://publicsuffix.org/learn/
-		if url.contains("tz.de") {
-			fixedURL = "http://tz.de"
-		}
-		if let domainName = self.domainName(fixedURL), //URLParser.getURLDetails(fixedURL),
-            let hostName = URL(string: fixedURL)?.host,
-			let db = self.logoDB,
-			db != JSON.null,
-			db["domains"] != JSON.null {
-			let details = db["domains"]
-			let host = details[domainName]
-			logoDetails.hostName = domainName
-			logoDetails.prefix = domainName.subSctring(to: min(2, domainName.count)).capitalized
-			if let list = host.array,
-				list.count > 0 {
-				for info in list {
-					if info != JSON.null,
-					   let r = info["r"].string,
-						isMatchingLogoRule(hostName, domainName, r) || info == list.last {
-
-						if let doesLogoExist = info["l"].number, doesLogoExist == 1 {
-							logoDetails.url = "https://cdn.cliqz.com/brands-database/database/\(self.dbVersion)/pngs/\(domainName)/\(r)_192.png"
-						}
-						logoDetails.color = info["b"].string
-						if let txt = info["t"].string {
-							logoDetails.prefix = txt
-						}
-                        if r != "$", let address = hostName.lastIndex(of: domainName) {
-                            logoDetails.hostName = hostName.subSctring(to: address + domainName.count)
-                        }
-						break
-					}
-				}
-			}
-		}
-		if logoDetails.color == nil {
-			logoDetails.color = "000000"
-			let palette = self.logoDB?["palette"]
-			if let list = palette?.array,
-				let asciiVal = logoDetails.hostName?.asciiValue() {
-				let idx = asciiVal % list.count
-				logoDetails.color = list[idx].string
-			}
-		}
-		return logoDetails
-	}
-	
-	private class func isMatchingLogoRule(_ host: String, _ domain: String, _ rule: String) -> Bool {
-        if let address = host.lastIndex(of: domain) {
-            let r = "\(host.subSctring(to: address))$\(host.subSctring(from: address + domain.count))"
-            return r.contains(rule)
-        }
-        let r = domain + ".$"
-        return r.contains(rule)
-
-	}
-
-	private class func domainName(_ urlString: String) -> String? {
-		if let url = NSURL(string: urlString),
-			let domain = url.host?.registeredDomain(),
-			let suffix = url.publicSuffix() {
-			let indx = domain.index(domain.endIndex, offsetBy: -(suffix.count + 1))
-			return domain.substring(to: indx)
-		}
-		return nil
-	}
-	
 	class func downloadImage(_ url: String, completed: @escaping (_ image: UIImage?, _ error:  Error?) -> Void) {
 		if let u = URL(string: url) {
 			SDWebImageManager.shared().loadImage(with: u, options: SDWebImageOptions.highPriority, progress: nil, completed: { (image, _, error, _, _, _) in
@@ -158,5 +83,77 @@ class LogoLoader {
 		}
 	}
 	
+    class func fetchLogoDetails(_ url: String) -> LogoInfo {
+        var logoDetails = LogoInfo()
+        logoDetails.color = nil
+        logoDetails.fontSize = 16
+        var fixedURL = url
+        // TODO: Remove this crazy hack, which is done for localNews. For the next release we should change url parsing lib to https://publicsuffix.org/learn/
+        if url.contains("tz.de") {
+            fixedURL = "http://tz.de"
+        }
+        if let domainName = self.domainName(fixedURL), //URLParser.getURLDetails(fixedURL),
+            let hostName = URL(string: fixedURL)?.host,
+            let db = self.logoDB,
+            db != JSON.null,
+            db["domains"] != JSON.null {
+            let details = db["domains"]
+            let host = details[domainName]
+            logoDetails.hostName = domainName
+            logoDetails.prefix = domainName.subString(to: min(2, domainName.count)).capitalized
+            if let list = host.array,
+                list.count > 0 {
+                let domainRule = generateDomainRule(hostName, domainName)
+                for info in list {
+                    if info != JSON.null,
+                        let r = info["r"].string,
+                        domainRule.contains(r) || info == list.last {
+                        
+                        if let doesLogoExist = info["l"].number, doesLogoExist == 1 {
+                            logoDetails.url = "https://cdn.cliqz.com/brands-database/database/\(self.dbVersion)/pngs/\(domainName)/\(r)_192.png"
+                        }
+                        logoDetails.color = info["b"].string
+                        if let txt = info["t"].string {
+                            logoDetails.prefix = txt
+                        }
+                        if r != "$", let address = hostName.lastIndex(of: domainName) {
+                            logoDetails.hostName = hostName.subString(to: address + domainName.count)
+                        }
+                        break
+                    }
+                }
+            }
+        }
+        if logoDetails.color == nil {
+            logoDetails.color = "000000"
+            let palette = self.logoDB?["palette"]
+            if let list = palette?.array,
+                let asciiVal = logoDetails.hostName?.asciiValue() {
+                let idx = asciiVal % list.count
+                logoDetails.color = list[idx].string
+            }
+        }
+        return logoDetails
+    }
+    
+    private class func generateDomainRule(_ host: String, _ domain: String) -> String {
+        if let address = host.lastIndex(of: domain) {
+            let rule = "\(host.subString(to: address))$\(host.subString(from: address + domain.count))"
+            return rule
+        }
+        return "\(domain).$"
+        
+    }
+    
+    class func domainName(_ urlString: String) -> String? {
+        if let url = NSURL(string: urlString),
+            let domain = url.host?.registeredDomain(),
+            let suffix = url.publicSuffix() {
+            let domainName = domain.subString(to: domain.count - suffix.count - 1)
+            return domainName
+        }
+        return nil
+    }
+    
 }
 
