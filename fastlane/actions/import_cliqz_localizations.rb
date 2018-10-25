4601eb5590bff8f3833921dd0a731306561f3037
Mahmoud Adam <mahmoud@cliqz.com>
2018-10-25 16:13:17 +0200

    Inject App name while importing localized strings

--- fastlane/actions/import_cliqz_localizations.rb
+++ fastlane/actions/import_cliqz_localizations.rb
@@ -9,13 +9,16 @@ module Fastlane
         UI.message "Parameter cliqz l10n Folder: #{params[:l10n_folder]}"
   
         #import Cliqz localized strings
-        self.import_localization(params[:l10n_folder], "cliqz-ios.xliff")
+        self.import_localization(params[:l10n_folder], params[:app_name], "cliqz-ios.xliff")
       end
 
-      def self.import_localization(directory, xliff_name)
+      def self.import_localization(directory, app_name, xliff_name)
         command_line = ""
         for language in SUPPORTED_LANGUAGES do
-          command_line = "xcodebuild -importLocalizations -localizationPath #{directory}/#{language}/#{xliff_name} -project Client.xcodeproj"
+          filePath = "#{directory}/#{language}/#{xliff_name}"
+          Actions.sh("source import-locales-env/bin/activate && fastlane/scripts/inject_app_name.py #{filePath} #{app_name}")
+
+          command_line = "xcodebuild -importLocalizations -localizationPath #{filePath} -project Client.xcodeproj"
           UI.message("import xliff file at #{directory}/#{language}/#{xliff_name}")
           Actions.sh(command_line)
         end
@@ -40,6 +43,13 @@ module Fastlane
                                        description: "path to get CLiqz locatlization xliff files", # a short description of this parameter
                                        verify_block: proc do |value|
                                           raise "No Cliqz l10 folder for xliff files given, pass using `l10n_folder: 'path'`".red unless (value and not value.empty?)
+                                          # raise "Couldn't find file at path '#{value}'".red unless File.exist?(value)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :app_name,
+                                       env_name: "FL_IMPORT_BUILD_TOOLS_CLIQZ_APP_NAME", # The name of the environment variable
+                                       description: "Localized app name", # a short description of this parameter
+                                       verify_block: proc do |value|
+                                          raise "No Cliqz App name given, pass using `app_name: 'name'`".red unless (value and not value.empty?)
                                           # raise "Couldn't find file at path '#{value}'".red unless File.exist?(value)
                                        end)
         ]
