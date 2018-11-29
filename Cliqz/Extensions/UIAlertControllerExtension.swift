40ba12673e6c880e45f06f598412b15a6b524560
Tim Palade <timotei@cliqz.com>
2018-11-29 16:22:02 +0100

    Add alert heper functions

--- Cliqz/Extensions/UIAlertControllerExtension.swift
+++ Cliqz/Extensions/UIAlertControllerExtension.swift
@@ -16,4 +16,37 @@ extension UIAlertController {
             preferredStyle: .alert
         )
     }
+    
+    class func alertWithCancelAndAction(text: String, actionButtonTitle: String, actionCallback: @escaping (UIAlertAction) -> Void) -> UIAlertController {
+        let alert = UIAlertController(
+            title: "",
+            message: text,
+            preferredStyle: .alert
+        )
+        
+        let cancel = UIAlertAction(title: NSLocalizedString("Cancel", tableName: "Cliqz", comment: "Cancel button title in the urlbar"), style: .destructive, handler: nil)
+        
+        let action = UIAlertAction(
+            title: actionButtonTitle,
+            style: .default,
+            handler: actionCallback
+        )
+        
+        alert.addAction(cancel)
+        alert.addAction(action)
+        return alert
+    }
+    
+    class func alertWithOkay(text: String) -> UIAlertController {
+        let alert = UIAlertController(
+            title: "",
+            message: text,
+            preferredStyle: .alert
+        )
+        
+        let okay = UIAlertAction(title: NSLocalizedString("Okay", tableName: "Cliqz", comment: "Okay button for alerts"), style: .destructive, handler: nil)
+        
+        alert.addAction(okay)
+        return alert
+    }
 }
