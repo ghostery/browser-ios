fff0d437c74df32089f112672f9c6e1935503240
giota-cliqz <panagiota@cliqz.com>
2019-01-14 17:10:51 +0100

    EX-8495: fix for video downloader & update to latest @cliqz/indexeddbshim

--- setup.js
+++ setup.js
@@ -1,4 +1,8 @@
 import buffer from 'buffer';
+import UserAgent from 'react-native-user-agent';
+import defineEnumerableProperties from '@babel/runtime/helpers/defineEnumerableProperties';
+
+Object.assign(babelHelpers, { defineEnumerableProperties });
 
 if (!global.Buffer) {
   global.Buffer = buffer.Buffer;
@@ -7,3 +11,5 @@ if (!global.Buffer) {
 if (!process.version) {
   process.version = '';
 }
+
+global.navigator.userAgent = navigator.userAgent || UserAgent.getUserAgent();
