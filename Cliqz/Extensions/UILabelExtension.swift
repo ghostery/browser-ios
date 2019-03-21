a68e01e3a90a897d978174ca88f7c12c61ae92a3
Mahmoud Adam <mahmoud@cliqz.com>
2019-03-21 16:48:11 +0100

    [IP-317] Added "Made in Germany" watermark

--- Cliqz/Extensions/UILabelExtension.swift
+++ Cliqz/Extensions/UILabelExtension.swift
@@ -17,4 +17,11 @@ extension UILabel {
         self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
     }
     
+    func addCharacterSpacing(kernValue: Double = 1.15) {
+        if let labelText = text, labelText.count > 0 {
+            let attributedString = NSMutableAttributedString(string: labelText)
+            attributedString.addAttribute(NSAttributedStringKey.kern, value: kernValue, range: NSRange(location: 0, length: attributedString.length - 1))
+            attributedText = attributedString
+        }
+    }
 }
