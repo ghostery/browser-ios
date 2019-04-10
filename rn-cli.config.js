6bbd62deafc6e283fc8b0f70d4fe25ea68c8d854
giota-cliqz <panagiota@cliqz.com>
2019-04-10 17:46:54 +0200

    update to latest react-native

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,11 +1,13 @@
 // From https://github.com/webpack/node-libs-browser and https://github.com/parshap/node-libs-react-native
 module.exports = {
-  extraNodeModules: {
-    fs: require.resolve('./_empty'),
-    stream: require.resolve('stream-browserify'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    path: require.resolve('path-browserify'),
-    buffer: require.resolve('buffer'),
+  resolver: {
+    extraNodeModules: {
+      fs: require.resolve('./_empty'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      path: require.resolve('path-browserify'),
+      buffer: require.resolve('buffer'),
+    },
   },
 };
