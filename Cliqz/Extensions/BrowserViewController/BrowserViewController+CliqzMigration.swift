3a6c4ba534efc850356b9eeff3c9afdfca1a7895
Naira <naira@cliqz.com>
2019-06-27 17:33:12 +0200

    [IB-1891]: Inform users about change

---
+++ Cliqz/Extensions/BrowserViewController/BrowserViewController+CliqzMigration.swift
@@ -0,0 +1,44 @@
+//
+//  BrowserViewController+CliqzMigration.swift
+//  Client
+//
+//  Created by Sahakyan on 6/27/19.
+//  Copyright © 2019 Cliqz. All rights reserved.
+//
+
+import Foundation
+import Shared
+
+extension BrowserViewController {
+	private static let updateSeenKey = "CliqzUpdateInfoPageSeen"
+
+	func showCliqzUpdateInfoPageFirstTime() {
+		guard isExistingUser(), !isUpdateInfoPageSeen() else {
+			return
+		}
+		let urlStr = "http://cliqz.com/en/magazine/cliqz-ios-update"
+		if let url = URL(string: urlStr) {
+			self.openURLInNewTab(url, isPrivileged: false)
+			self.markUpdateInfoPageSeen()
+		}
+	}
+
+	private func isExistingUser() -> Bool {
+		guard let introSeen = profile.prefs.intForKey(PrefsKeys.IntroSeen), introSeen == 1 else {
+			return false
+		}
+		return true
+	}
+
+	private func isUpdateInfoPageSeen() -> Bool {
+		if let _ = LocalDataStore.value(forKey: BrowserViewController.updateSeenKey) {
+			return true
+		}
+		return false
+	}
+
+	private func markUpdateInfoPageSeen() {
+		LocalDataStore.set(value: true, forKey: BrowserViewController.updateSeenKey)
+	}
+
+}
