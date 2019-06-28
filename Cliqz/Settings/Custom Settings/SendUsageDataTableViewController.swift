0bae4e2e3ab1608bbbfe2dd4c7606610e695f43f
Naira <naira@cliqz.com>
2019-06-28 11:02:12 +0200

    [IB-1886]: [Rebranding] Replace Ghostery strings with Cliqz strings

--- Cliqz/Settings/Custom Settings/SendUsageDataTableViewController.swift
+++ Cliqz/Settings/Custom Settings/SendUsageDataTableViewController.swift
@@ -25,11 +25,13 @@ class SendUsageDataTableViewController: ToggleSubSettingsTableViewController {
     override func getSectionFooters() -> [String] {
         #if PAID
         return [NSLocalizedString("Help us improve your browsing experience. Lumen collects strictly anonymous usage data. At no occasion is any personal data collected.", tableName: "Lumen", comment: "Restore Tabs Prompt Description")]
+		#elseif CLIQZ
+		return [NSLocalizedString("Help us improve your browsing experience. Cliqz collects strictly anonymous usage data. At no occasion is any personal data collected.", tableName: "Cliqz", comment: "Restore Tabs Prompt Description")]
         #else
         return [NSLocalizedString("Help us improve your browsing experience. Ghostery collects strictly anonymous usage data. At no occasion is any personal data collected.", tableName: "Ghostery", comment: "Restore Tabs Prompt Description")]
         #endif
     }
-    
+
     override func saveToggles(isOn: Bool, atIndex: Int) {
         SettingsPrefs.shared.updateSendUsageDataPref(isOn)
     }
