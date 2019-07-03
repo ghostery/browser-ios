41eac849553021f03e453416c8972a8d0676c9de
Khaled Tantawy <khaled@cliqz.com>
2019-07-03 13:40:42 +0200

    Vertical Cards WIP

---
+++ cliqzWrapper.js
@@ -0,0 +1,25 @@
+import inject from 'browser-core-lumen-ios/build/modules/core/kord/inject';
+
+const callAction = (module, action, ...args) => inject.module(module).action(action, ...args);
+const createModuleWrapper = (module, actions) =>
+  actions.reduce((all, action) =>
+    ({ ...all, [action]: callAction.bind(null, module, action) }), {});
+
+export default class Cliqz {
+  constructor() {
+    this.mobileCards = createModuleWrapper('mobile-cards', [
+      'openLink',
+      'callNumber',
+      'openMap',
+      'hideKeyboard',
+      'sendUIReadySignal',
+      'handleAutocompletion',
+      'getConfig',
+      'getTrackerDetails',
+    ]);
+
+    this.core = createModuleWrapper('core', []);
+    this.search = createModuleWrapper('search', ['getSnippet', 'reportHighlight']);
+    this.geolocation = createModuleWrapper('geolocation', ['updateGeoLocation']);
+  }
+}
