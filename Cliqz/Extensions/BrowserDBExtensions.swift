d62be0f5da0b4b66ca8684aaff75af3ac69004f4
Daniel Jilg <winsmith@winsmith.de>
2019-07-17 15:31:38 +0200

    Get all unmigrated bookmarks

--- Cliqz/Extensions/BrowserDBExtensions.swift
+++ Cliqz/Extensions/BrowserDBExtensions.swift
@@ -27,23 +27,35 @@ extension BrowserDB {
     /// `bookmarksLocal.bookmarked_date` which was introduced by Old Cliqz Mobile for this reason. We translate that
     /// sorting into `bookmarksLocalStructure.idx`, which seems intended for sorting by Firefox Mobile.
     private func migrateCliqzBookmarks() {
-        // TODO: Get all unmigrated bookmarks, sorted by bookmarked_date
-        let getUnmigratedBookmarksSQL = """
-            SELECT * FROM 'bookmarksLocal'
-            LEFT JOIN bookmarksLocalStructure ON bookmarksLocal.guid = bookmarksLocalStructure.child
+        // Get all unmigrated bookmarks, sorted by bookmarked_date
+        let fetchUnmigratedBookmarksSQL = """
+            SELECT * FROM \(TableBookmarksLocal)
+            LEFT JOIN \(TableBookmarksLocalStructure) ON \(TableBookmarksLocal).guid = \(TableBookmarksLocalStructure).child
             WHERE parentid = 'mobile______'
             AND child is NULL
             ORDER BY bookmarked_date
             """
+        let results = self.runQuery(fetchUnmigratedBookmarksSQL, args: nil, factory: BookmarkFactory.factory).value
+        guard let oldBookmarks = results.successValue else { return }
 
-        // TODO: Get highest idx value to add on top
-        let getHighestIDXValueForMobileSQL = """
-            SELECT idx FROM bookmarksLocalStructure
+        // Get highest idx value to add on top
+        let fetchHighestIDXValueForMobileSQL = """
+            SELECT idx FROM \(TableBookmarksLocalStructure)
             WHERE parent =  "mobile______"
             ORDER BY idx DESC
             LIMIT 1
             """
+        guard let highestIDXResults = self.runQuery(fetchHighestIDXValueForMobileSQL, args: nil, factory: BrowserDB.idxFactory).value.successValue else { return }
+        var highestIDX = 0
+        for value in highestIDXResults where value != nil { highestIDX = value! }
+
+        print(highestIDX)
+
 
         // TODO: Create entries to bookmarksLocalStructure with updated idx's
+    }
+
+    private class func idxFactory(_ row: SDRow) -> Int {
+        return row["idx"] as! Int
     }
 }
